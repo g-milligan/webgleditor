373de7052a42fd5a3cfa202952ac5c3f7798b6ee
g-milligan <gmilligan@human-element.com>
2016-04-23 11:58:54 -0400

    key events for left nav

--- js/app/plugins/filenav.js
+++ js/app/plugins/filenav.js
@@ -1,5 +1,20 @@
 var filenav=(function(){
   return{
+    focusNext:function(wrap){
+
+    },
+    focusPrev:function(wrap){
+
+    },
+    clearFocus:function(wrap){
+      wrap.removeClass('focus');
+      wrap.find('li.focus').removeClass('focus');
+    },
+    setFocus:function(li){
+      var wrap=li.parents('.init_filenav_wrap:first');
+      wrap.find('li.focus').removeClass('focus');
+      li.addClass('focus');
+    },
     openItem:function(li){
       //if this is a parent dir
       if(li.children('ul:last').length>0){
@@ -30,6 +45,48 @@ var filenav=(function(){
         if(filenav.length<1){
           wrap.append('<div class="init_filenav_wrap"><ul></ul></div>');
           filenav=wrap.children('.init_filenav_wrap');
+          filenav.hover(function(){
+            jQuery(this).addClass('focus');
+          },function(){
+            jQuery(this).removeClass('focus');
+          });
+          filenav.mouseleave(function(){
+            jQuery(this).removeClass('focus');
+          });
+          jQuery(document).keydown(function(e){
+            var focusLis=jQuery('.init_filenav_wrap li.focus');
+            if(focusLis.length>0){
+              if(focusLis.length>1){ focusLis=focusLis.eq(0); }
+              var nav=focusLis.parents('.init_filenav_wrap:first');
+              switch(e.keyCode){
+                case 38: //key up
+                  e.preventDefault(); e.stopPropagation();
+                  self['focusPrev'](nav);
+                  break;
+                case 40: //key down
+                  e.preventDefault(); e.stopPropagation();
+                  self['focusNext'](nav);
+                  break;
+                case 13: //enter key
+                  e.preventDefault(); e.stopPropagation();
+                  nav.find('li.focus:first').children('.lbl:first').click();
+                  break;
+                case 27: //escape key
+                  e.preventDefault(); e.stopPropagation();
+                  self['clearFocus'](nav);
+                  break;
+              }
+            }
+          });
+          jQuery(document).click(function(e){
+            var nav=jQuery('.init_filenav_wrap');
+            if(nav.length>0){
+              e.preventDefault(); e.stopPropagation();
+              nav.each(function(){
+                self['clearFocus'](jQuery(this));
+              });
+            }
+          });
         }
         var rootUl=filenav.children('ul:first');
         //default path values
@@ -51,16 +108,20 @@ var filenav=(function(){
               if(toggle.length<1){
                 li.prepend('<span class="toggle"></span>');
                 toggle=li.children('span.toggle:first');
-                toggle.click(function(){
-                  self['toggleItem'](jQuery(this).parents('li:first'));
+                toggle.click(function(e){
+                  e.preventDefault(); e.stopPropagation();
+                  var l=jQuery(this).parents('li:first');
+                  self['toggleItem'](l); self['setFocus'](l);
                 });
                 var otherTriggers=li.children('.ico:first,.lbl:first');
                 otherTriggers.each(function(){
                   if(!jQuery(this)[0].hasOwnProperty('init_toggle_open_trigger')
                   || !jQuery(this)[0]['init_toggle_open_trigger']){
                     jQuery(this)[0]['init_toggle_open_trigger']=true;
-                    jQuery(this).click(function(){
-                      self['toggleItem'](jQuery(this).parents('li:first'));
+                    jQuery(this).click(function(e){
+                      e.preventDefault(); e.stopPropagation();
+                      var l=jQuery(this).parents('li:first');
+                      self['toggleItem'](l); self['setFocus'](l);
                     });
                   }
                 });
@@ -99,6 +160,7 @@ var filenav=(function(){
               if(existingIco.length<1){
                 li.prepend('<span class="ico">'+json['svg']+'</span>');
               }
+              setToggleopen(li);
               return li;
             };
             var path=data['path'];
