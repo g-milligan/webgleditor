4ea9a7754fff36746096420485bf99a2046398c8
g-milligan <gmilligan@human-element.com>
2016-04-26 09:48:48 -0400

    appdata element maps

--- js/app/appdata.js
+++ js/app/appdata.js
@@ -1,5 +1,12 @@
 var appdata=(function(){
   return{
+    map:{
+      startup_template:{
+        value:function(self, el){
+          return self['getCommentContent'](el.text());
+        }
+      }
+    },
     getCommentContent:function(str){
       str=str.trim(); var hasComment=false;
       if(str.indexOf('<!--')===0){
@@ -11,44 +18,40 @@ var appdata=(function(){
       if(hasComment){ str=str.trim(); }
       return str;
     },
-    get:function(){
+    get:function(getId){
       var ret, self=this;
       var appDataEl=jQuery('#app_data:first');
       if(appDataEl.length>0){
-        ret={};
-        appDataEl.children('[id]').each(function(){
-          var div=jQuery(this);
-          var id=div.attr('id');
-          switch(id){
-            case 'startup_template':
-              ret[id]=self['getCommentContent'](div.text());
-              break;
-            case 'templates':
-
-
-
-
-
-
-              break;
-            case 'data_file_settings':
-
-
-
-
-
-
-              break;
-            case 'template_inserts':
-
-
-
-
-
-            
-              break;
+        //function to get the mapped value for the given id
+        var getMappedValue=function(id){
+          var val;
+          var mapItem=self['map'][id];
+          var itemSelector='#'+id;
+          if(mapItem.hasOwnProperty('selector')){
+            itemSelector=mapItem['selector'];
           }
-        });
+          var el=appDataEl.find(itemSelector);
+          if(el.length>0){
+            if(mapItem.hasOwnProperty('value')){
+              val=mapItem['value'](self, el);
+            }
+          }
+          return val;
+        };
+        //if no specific id is specified
+        if(getId==undefined){
+          ret={};
+          //cycle through each mapped element and return all of the values
+          for(var id in self['map']){
+            if(self['map'].hasOwnProperty(id)){
+              var val=getMappedValue(id);
+              if(val!=undefined){ret[id]=val;}
+            }
+          }
+        }else{
+          //a specific getId is specified
+          var ret=getMappedValue(getId);
+        }
       } return ret;
     }
   };
