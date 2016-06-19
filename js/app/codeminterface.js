5843164cd560cdc6acd765d16f6f9a6db9e4888d
g-milligan <gmilligan@human-element.com>
2016-06-19 19:42:05 -0400

    content text and theme

--- js/app/codeminterface.js
+++ js/app/codeminterface.js
@@ -1,6 +1,23 @@
 var codeminterface=(function(){
   return{
-    getFileWrap:function(path, createIfNew){
+    getFileMode:function(path){
+      var ext=this['getExtension'](path); var mode;
+      switch(ext){
+        case 'html': mode='htmlmixed'; break;
+      }
+      return mode;
+    },
+    getExtension:function(str){
+      var ret='';
+      if(str.indexOf('.')!==-1){
+        ret=str;
+        ret=ret.substring(ret.lastIndexOf('.')+'.'.length);
+        ret=ret.toLowerCase();
+        ret=ret.trim();
+      }
+      return ret;
+    },
+    getFileWrap:function(path, createIfNew, content){
       var self=this;
       if(createIfNew==undefined){ createIfNew=false; }
       var mainPanel=jQuery('#main_panel_center:first');
@@ -9,15 +26,26 @@ var codeminterface=(function(){
       //this file wrap not yet created
       if(fileWrap.length<1){
         if(createIfNew){
-          panelContent.append('<div class="content" data-file="'+path+'"></div>');
-          fileWrap=panelContent.children('.content[data-file="'+path+'"]:first');
-
-
-          //**** create the code mirror textarea, etc...
-
-
-
-
+          panelContent.append('<div class="content" data-file="'+path+'"><textarea class="content"></textarea></div>');
+          fileWrap=panelContent.children('.content[data-file="'+path+'"]:last');
+          var textarea=fileWrap.children('textarea.content:first');
+          //set the codemirror config options
+          var config={};
+          if(content==undefined){ content=''; }
+          config['value']=content;
+          config['lineNumbers']=true;
+          config['extraKeys']={
+            'Ctrl-Space':'autocomplete'
+          };
+          config['styleActiveLine']=true;
+          config['mode']=self['getFileMode'](path);
+          config['theme']='custom-dark';
+          //init the editor textarea
+          var myCodeMirror = CodeMirror(function(el) {
+            //textarea.removeClass('raw');
+            textarea[0].parentNode.replaceChild(el, textarea[0]);
+          },config);
+          //****
         }else{
           fileWrap=undefined;
         }
@@ -32,7 +60,7 @@ var codeminterface=(function(){
       var content=template_parser['getDataFileContent'](fullContent['content'], path);
       if(content!=undefined){
         //build the html for this file, if not already built
-        var fileWrap=self['getFileWrap'](strPath, true);
+        var fileWrap=self['getFileWrap'](strPath, true, content);
         //open this content if it's not already active
         if(!fileWrap.hasClass('active')){
           var panelContent=fileWrap.parent();
